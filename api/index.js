fe55b3e178e03ceb4ac1a2777d12e32c13d7a1af
unaizakm-svg <unaizakm@gmail.com>
2026-05-25 20:23:45 +0500

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,8 +1,54 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
 
 app.get("/", (req, res) => {
   res.send("Backend is running 👍");
+});
+
+app.post("/contact", async (req, res) => {
+
+  try {
+
+    await client.connect();
+
+    const db = client.db("textileDB");
+
+    const contacts = db.collection("contacts");
+
+    await contacts.insertOne({
+
+      name: req.body.name,
+
+      email: req.body.email,
+
+      message: req.body.message
+
+    });
+
+    res.send("Feedback submitted successfully ✔️");
+
+  }
+
+  catch (err) {
+
+    console.log(err);
+
+    res.send("Error saving data");
+
+  }
+
 });
 
 module.exports = app;
